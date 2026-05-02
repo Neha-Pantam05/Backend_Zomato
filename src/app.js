9b2c2dcc27a401d243adb48d18957fc8d5a07a02
NEHA PANTAM <pantamneha@gmail.com>
2026-05-02 19:47:45 +0530

    cors fixed

--- src/app.js
+++ src/app.js
@@ -10,7 +10,10 @@ const cors = require("cors")
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://frontend-zomato.vercel.app"
+  ],
   credentials: true
 }))
 
